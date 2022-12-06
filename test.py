9a11609b5a6a78076757a070d0dd5902f8716b16
Shelton Ngwenya <shelt15.nn@gmail.com>
2022-12-06 14:14:51 +0000

    Small changes

--- test.py
+++ test.py
@@ -10,10 +10,9 @@ import COMP7039_CI_Code_BaseV1_0
 class Test_COMP7039_CI_Code_BaseV1_0(unittest.TestCase):
 
     def test_read_integer_between_numbers(self):
-        result = COMP7039_CI_Code_BaseV1_0.read_integer_between_numbers(prompt="Number is not between 1 and 7",
-                                                                        mini=1,
-                                                                        maximum=7)
-        self.assertIn(result, 7 <= result <= 1)
+        result = COMP7039_CI_Code_BaseV1_0.read_integer_between_numbers(
+            prompt="Number is not between 1 and 7",                                                  mini=1,                                                maximum=7)
+        self.assertIn(result, 7 >= result >= 1)
 
 
 if __name__ == 'main':
